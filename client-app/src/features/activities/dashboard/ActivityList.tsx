1a88e0322cdf10567230422684dfd25f8a305d8e
Mahesh Parale <maheshparale10@gmail.com>
2020-07-19 16:53:49 +0530

    Finishing up with client side activity dashboard

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,58 +1,53 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import { Item, Button, Label, Segment } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
-import ActivityListItem from './ActivityListItem';
-import { ActivityDetails } from './ActivityDetails';
-import { Paper } from '@material-ui/core';
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-    },
-    image: {
-      width: 128,
-      height: 128,
-    },
-    img: {
-      margin: "auto",
-      display: "block",
-      maxWidth: "100%",
-      maxHeight: "100%",
-    },
-    details: {
-      justifyContent: "center"
-    }
-  })
-);
 
 interface IProps {
-  activities: IActivity,
+  activities: IActivity[];
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
 }
 
-export const ActivityList = ({activities}) => {
-  const classes = useStyles();
-
-  const gridItems = activities.map(a =>
-    <Grid item xs={12}>
-      <ActivityListItem activity={a}></ActivityListItem>
-    </Grid>
-  );
-
+const ActivityList: React.FC<IProps> = ({
+  activities,
+  selectActivity,
+  deleteActivity
+}) => {
   return (
-    <div className={classes.root}>
-        <Grid container>
-          <Grid item container xs={12} md={4}>
-            {gridItems}
-          </Grid>
-          <Grid className={classes.details} item container xs={12} md={8}>
-            <ActivityDetails></ActivityDetails>
-          </Grid> 
-        </Grid>
-    </div>
+    <Segment clearing>
+      <Item.Group divided>
+        {activities.map(activity => (
+          <Item key={activity.id}>
+            <Item.Content>
+              <Item.Header as='a'>{activity.title}</Item.Header>
+              <Item.Meta>{activity.date}</Item.Meta>
+              <Item.Description>
+                <div>{activity.description}</div>
+                <div>
+                  {activity.city}, {activity.venue}
+                </div>
+              </Item.Description>
+              <Item.Extra>
+                <Button
+                  onClick={() => selectActivity(activity.id)}
+                  floated='right'
+                  content='View'
+                  color='blue'
+                />
+                <Button
+                  onClick={() => deleteActivity(activity.id)}
+                  floated='right'
+                  content='Delete'
+                  color='red'
+                />
+                <Label basic content={activity.category} />
+              </Item.Extra>
+            </Item.Content>
+          </Item>
+        ))}
+      </Item.Group>
+    </Segment>
   );
-}
+};
+
+export default ActivityList;
--- client-app/src/features/nav/NavBar.tsx
+++ client-app/src/features/nav/NavBar.tsx
@@ -1,44 +1,25 @@
-import React from 'react'
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
 
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }),
-);
-
-export const NavBar = () => {
-    const classes = useStyles();
-
-    return (
-        <div className={classes.root}>
-            <AppBar position="sticky">
-                <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                    <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" className={classes.title}>
-                        Reactivities
-                    </Typography>
-                    <Button color="inherit">Activities</Button>
-                    <Button color="inherit">Create Activity</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    )
+interface IProps {
+  openCreateForm: () => void;
 }
+
+const NavBar: React.FC<IProps> = ({openCreateForm}) => {
+  return (
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item header>
+            <img src="/assets/logo.png" alt="logo" style={{marginRight: 10}}/>
+            Reactivities
+        </Menu.Item>
+        <Menu.Item name='Activities' />
+        <Menu.Item>
+            <Button onClick={openCreateForm} positive content='Create Activity' />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
